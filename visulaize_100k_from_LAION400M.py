8749a2718f697cbdc04b9e856b164a3f2724c1ac
Raghav-B <raghavbhardwaj1998@gmail.com>
2026-01-08 08:59:09 +0800

    refactor to single MLP module

--- visulaize_100k_from_LAION400M.py
+++ visulaize_100k_from_LAION400M.py
@@ -17,7 +17,7 @@ from torchvision import datasets, transforms
 import tqdm
 
 from os.path import join
-from datasets import load_dataset
+# from datasets import load_dataset
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 import json
@@ -27,52 +27,7 @@ import clip
 
 from PIL import Image, ImageFile
 
-
-# if you changed the MLP architecture during training, change it also here:
-
-class MLP(pl.LightningModule):
-    def __init__(self, input_size, xcol='emb', ycol='avg_rating'):
-        super().__init__()
-        self.input_size = input_size
-        self.xcol = xcol
-        self.ycol = ycol
-        self.layers = nn.Sequential(
-            nn.Linear(self.input_size, 1024),
-            #nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(1024, 128),
-            #nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(128, 64),
-            #nn.ReLU(),
-            nn.Dropout(0.1),
-
-            nn.Linear(64, 16),
-            #nn.ReLU(),
-
-            nn.Linear(16, 1)
-        )
-
-    def forward(self, x):
-        return self.layers(x)
-
-    def training_step(self, batch, batch_idx):
-            x = batch[self.xcol]
-            y = batch[self.ycol].reshape(-1, 1)
-            x_hat = self.layers(x)
-            loss = F.mse_loss(x_hat, y)
-            return loss
-    
-    def validation_step(self, batch, batch_idx):
-        x = batch[self.xcol]
-        y = batch[self.ycol].reshape(-1, 1)
-        x_hat = self.layers(x)
-        loss = F.mse_loss(x_hat, y)
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer
+from mlp import MLP
 
 def normalized(a, axis=-1, order=2):
     import numpy as np  # pylint: disable=import-outside-toplevel
